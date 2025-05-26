cd3e74ff288e59512f8cc897ba1d4ba29d8a6266
jpat64 <jimmy@pattersonadvice.com>
2025-05-26 17:25:18 -0500

    better-bracket-zero

--- bracket_zero.py
+++ bracket_zero.py
@@ -2,6 +2,7 @@ from random import random
 from math import floor
 from requests import api
 from time import sleep
+import json
 
 class Card:
     def __init__(self, name, quantity):
@@ -16,7 +17,7 @@ def toMTGA(commander, deck, number):
     spacingPart = "\n\n"
     commanderPart = "Commander\n"
     deckTitlePart = "Deck\n"
-    deckPart = map(str, deck).join("\n")
+    deckPart = str.join("\n", list(map(str, deck)))
     return f"{aboutPart}Player {number}'s Deck{spacingPart}{commanderPart}{commander}{spacingPart}{deckTitlePart}{deckPart}"
      
 
@@ -24,66 +25,63 @@ def main():
     deck = []
     added_cards = 0
 
-    # first, pick a commander from a predetermined list
-    valid_commander_names = [
-        "Atogatog", 
-        "Cromat", 
-        "Marina Vendrell", 
-        "Rukarumel, Biologist", 
-        "Progenitus", 
-        "Sliver Hivelord", 
-        "Tom Bombadil",
-        "Morophon, the Boundless", 
-        "Reaper King", 
-        "Ulalek, Fused Atrocity",
-        "Surgeon General Commander", 
-        "Tazri, Beacon of Unity"
-    ]
+    if True:
+        # first, pick a commander from a predetermined list
+        valid_commander_names = [
+            "Atogatog", 
+            "Cromat", 
+            "Marina Vendrell", 
+            "Rukarumel, Biologist", 
+            "Progenitus", 
+            "Sliver Hivelord", 
+            "Tom Bombadil",
+            "Morophon, the Boundless", 
+            "Reaper King", 
+            "Ulalek, Fused Atrocity",
+            "Surgeon General Commander", 
+            "Tazri, Beacon of Unity"
+        ]
 
-    choice = valid_commander_names[floor(random() * len(valid_commander_names))]
-    print("Added " + str(choice))
-    commander = Card(name=choice, quantity=1)
-    added_cards = 1
+        choice = valid_commander_names[floor(random() * len(valid_commander_names))]
+        print("Added " + str(choice))
+        commander = Card(name=choice, quantity=1)
+        added_cards = 1
 
-    base_url = "https://api.scryfall.com/cards/random"
-    card_extension = "?q=-type%3Aland%20and%20f%3acommander"
-    land_extension = "?q=type%3Aland%20and%20f%3acommander"
-
-    while(added_cards < 62):
-        response = api.get(f"{base_url}{card_extension}")       
-        if response.status_code >= 400:
-            print(f'bad response for random: {response.status_code}: {response.text}')
-            return
+        cards = open("default-cards.json", 'r', encoding='utf-8')
         
+        json_of_cards = json.load(cards)
 
-        name = response.json()["name"]
-        print("Added " + str(name))
-        print("\t(Took " +str(response.elapsed) + "... time)")
-        deck.append(Card(name=name, quantity=1))
-        added_cards += 1
-        sleep(0.1) # min. 100 milliseconds between requests
+        excluded_card_names = ["Plains", "Island", "Swamp", "Mountain", "Forest"]
+        excluded_card_names.append(choice)
+
+        while (added_cards < 85):
+            random_card = json_of_cards[floor(random() * len(json_of_cards))]
+            name = random_card["name"]
+            legal = random_card["legalities"]["commander"] == "legal"
+            if legal and (name not in excluded_card_names):
+                quantity = 1
+                deck.append(Card(name=name, quantity=quantity))
+                excluded_card_names.append(name)
+                added_cards += 1
+                print(f"Added {name}")
+            else:
+                print(f"Did not add {name} because it was either not legal or we already have one")
         
-    
-    while(added_cards < 85):
-        response = api.get(f"{base_url}{land_extension}")       
-        if response.status_code >= 400:
-            print(f'bad response for random: {response.status_code}: {response.text}')
-            return
-        
-        name = response.json()["name"]
-        print("Added " + str(name))
-        deck.append(Card(name=response.json()["name"], quantity=1))
-        added_cards += 1
-        sleep(0.1) # min. 100 milliseconds between requests
-    
-    deck.append(Card(name="Plains", quantity=3))
-    deck.append(Card(name="Island", quantity=3))
-    deck.append(Card(name="Swamp", quantity=3))
-    deck.append(Card(name="Mountain", quantity=3))
-    deck.append(Card(name="Forest", quantity=3))
-    print("Added 3 of each basic land")
-    
-    print(toMTGA(commander=commander, deck=deck, number=1))
+        deck.append(Card(name="Plains", quantity=2))
+        deck.append(Card(name="Island", quantity=2))
+        deck.append(Card(name="Swamp", quantity=2))
+        deck.append(Card(name="Mountain", quantity=2))
+        deck.append(Card(name="Forest", quantity=2))
+        deck.append(Card(name="Snow-Covered Plains", quantity=1))
+        deck.append(Card(name="Snow-Covered Island", quantity=1))
+        deck.append(Card(name="Snow-Covered Swamp", quantity=1))
+        deck.append(Card(name="Snow-Covered Mountain", quantity=1))
+        deck.append(Card(name="Snow-Covered Forest", quantity=1))
+
+        cards.close()
+
+        writefile = open("bracket_zero_list_1.txt", "w")
+        writefile.write(toMTGA(commander, deck, 1))
 
     
 main()
